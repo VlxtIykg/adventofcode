1d8d7052e64049798ffff51d2cbd73b3b8109669
Kami <harshimymommy@gmail.com>
2023-12-02 23:02:40 +0800

    feat: :tada: Part 1 of AOC Day 1

---
+++ AOC_solution/python/day1/part1.py
@@ -0,0 +1,27 @@
+# This is the Python solution for part 1 of day 1 of AOC.
+# part1.py
+from icecream import ic
+import re
+import setup   # noqa: F401
+import os
+
+file_path = os.getenv("day1textfile")  
+
+def get_first_and_last_numbers(string) -> tuple[int, int]:
+	numbers = re.findall(pattern=r'\d+', string=string)
+	compacted_numbers = "".join(numbers)
+	first_number = int(compacted_numbers[0])
+	last_number = int(compacted_numbers[-1])
+	concatenation = str(first_number) + str(last_number)
+	return int(concatenation)
+
+with open(file_path, "r") as file:
+	lines = file.readlines()
+	temp_ = []
+	for line in lines:
+		temp_.append(get_first_and_last_numbers(line))
+
+	lines = temp_
+	print(lines, )
+	ic(sum(lines))
+
