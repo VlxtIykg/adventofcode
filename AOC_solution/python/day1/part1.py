beb546b44bf8b9f4f0e7dc661298cd853164d602
Kami <harshimymommy@gmail.com>
2023-12-03 00:04:43 +0800

    style: 💄 Encapsulate opening file into function
    Signed-off-by: Kami <harshimymommy@gmail.com>

--- AOC_solution/python/day1/part1.py
+++ AOC_solution/python/day1/part1.py
@@ -1,5 +1,4 @@
-# This is the Python solution for part 1 of day 1 of AOC.
-# part1.py
+# This is my Python solution for part 1 of day 1 of AOC.
 from icecream import ic
 import re
 import setup   # noqa: F401
@@ -8,20 +7,26 @@ import os
 file_path = os.getenv("day1textfile")  
 
 def get_first_and_last_numbers(string) -> tuple[int, int]:
-	numbers = re.findall(pattern=r'\d+', string=string)
-	compacted_numbers = "".join(numbers)
-	first_number = int(compacted_numbers[0])
-	last_number = int(compacted_numbers[-1])
-	concatenation = str(first_number) + str(last_number)
-	return int(concatenation)
+  numbers = re.findall(pattern=r'\d+', string=string)
+  compacted_numbers = "".join(numbers)
+  if not compacted_numbers:
+    return 0
+  first_number = int(compacted_numbers[0])
+  last_number = int(compacted_numbers[-1])
+  concatenation = str(first_number) + str(last_number)
+  return int(concatenation)
 
-with open(file_path, "r") as file:
-	lines = file.readlines()
-	temp_ = []
-	for line in lines:
-		temp_.append(get_first_and_last_numbers(line))
 
-	lines = temp_
-	print(lines, )
-	ic(sum(lines))
+def read_file() -> None:
+  with open(file_path, "r") as file:
+    lines = file.readlines()
+    temp_ = []
+    for line in lines:
+      temp_.append(get_first_and_last_numbers(line))
 
+    lines = temp_
+    print(lines)
+    ic(sum(lines))
+    
+if __name__ == "__main__":
+  read_file()
