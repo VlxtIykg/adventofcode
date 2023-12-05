0fcab503fa52430460189bbd6cddfdb47ce17423
Kami <harshimymommy@gmail.com>
2023-12-06 00:58:49 +0800

    style(Styling): 💄 You get it, styling..
    Signed-off-by: Kami <harshimymommy@gmail.com>

--- AOC_solution/python/day1/part1.py
+++ AOC_solution/python/day1/part1.py
@@ -4,29 +4,31 @@ import re
 import setup   # noqa: F401
 import os
 
-file_path = os.getenv("day1textfile")  
+file_path = os.getenv("day1textfile")
+
 
 def get_first_and_last_numbers(string) -> tuple[int, int]:
-  numbers = re.findall(pattern=r'\d+', string=string)
-  compacted_numbers = "".join(numbers)
-  if not compacted_numbers:
-    return 0
-  first_number = int(compacted_numbers[0])
-  last_number = int(compacted_numbers[-1])
-  concatenation = str(first_number) + str(last_number)
-  return int(concatenation)
+    numbers = re.findall(pattern=r'\d+', string=string)
+    compacted_numbers = "".join(numbers)
+    if not compacted_numbers:
+        return 0
+    first_number = int(compacted_numbers[0])
+    last_number = int(compacted_numbers[-1])
+    concatenation = str(first_number) + str(last_number)
+    return int(concatenation)
 
 
 def read_file() -> None:
-  with open(file_path, "r") as file:
-    lines = file.readlines()
-    temp_ = []
-    for line in lines:
-      temp_.append(get_first_and_last_numbers(line))
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        temp_ = []
+        for line in lines:
+            temp_.append(get_first_and_last_numbers(line))
 
-    lines = temp_
-    print(lines)
-    ic(sum(lines))
-    
+        lines = temp_
+        print(lines)
+        ic(sum(lines))
+
+
 if __name__ == "__main__":
-  read_file()
+    read_file()
